b79daac587a7ebbcf85e0d6dca7d2f0e9801230b
Seohyun Kim <skim291@gmu.edu>
2025-06-17 16:17:31 +0900

    작업중03

--- src/worknest/cust_app.py
+++ src/worknest/cust_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
@@ -27,6 +28,32 @@ def register():
             'email': email
         })
     return redirect('/')
+# 고객사 목록 조회
+@app.route('/customers')
+def customer_list():
+    customers = list(collection.find())
+    return render_template('customer_list.html', customers=customers)
+
+# 고객사 정보 수정
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def edit_customer(id):
+    customer = collection.find_one({"_id": ObjectId(id)})
+    if request.method == 'POST':
+        collection.update_one({"_id": ObjectId(id)}, {
+            "$set": {
+                "name": request.form['name'],
+                "contact": request.form['contact'],
+                "email": request.form['email']
+            }
+        })
+        return redirect('/customers')
+    return render_template('edit_customer.html', customer=customer)
+
+# 고객사 삭제
+@app.route('/delete/<id>')
+def delete_customer(id):
+    collection.delete_one({"_id": ObjectId(id)})
+    return redirect('/customers')
 
 if __name__ == '__main__':
     app.run(debug=True)
